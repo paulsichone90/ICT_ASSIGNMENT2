6cd71c1dd47da8257600a5ffb55f10d6c3be6c9c
paulsichone90 <paulsichone000@gmail.com>
2026-04-15 15:13:51 +0200

    added comments

--- road.py
+++ road.py
@@ -1,17 +1,17 @@
-
-
-
-
+# accept user name
 name = input("Emter your name?: ")
+# accept user math score
 maths_score = int(input("Enter your maths result: "))
-
+#accept user bcs score
 bcs_score = int(input("Enter your bcs result: "))
-
+# accept user ict score
 ict_score = int(input("Enter your ict result: "))
 
+# calculate total and average scores;
 total = maths_score + bcs_score + ict_score
 average_score = total/ 3
 
+#print the results
 print(f"Name: {name}")
 print(f"Total: {total}")
 
